6af498b84ff855445348437293f17ee0f4109fd3
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-09-05 18:16:20 -0400

    Tabla de multiplicacion(C#)

---
+++ C#/Tabla de multiplicacion/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("reto tabla de multiplicar");
+int multiplicando = 0;
+int multiplicador = 0;
+Console.Write("hasta donde deseas tu tabla : ");
+int count = int.Parse(Console.ReadLine());
+while (multiplicador <= count)
+{
+    Console.WriteLine("\ntabla de el " + multiplicador + "\n");
+    for (multiplicando = 0; multiplicando <= 12; multiplicando++)
+    {
+        Console.WriteLine("{0} + {1} = {2}", multiplicando, multiplicador, multiplicando * multiplicador);
+
+    }
+    multiplicador++;
+
+
+
+
+}
