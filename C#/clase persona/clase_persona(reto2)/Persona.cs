fa0d87476cf8468f6da002ec91347bc37b7b6015
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2024-09-23 20:20:07 -0400

    reto2 (Fundamentos de C# para Principiantes )

---
+++ C#/clase persona/clase_persona(reto2)/Persona.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_persona_reto2_
+{
+    internal class Persona
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+
+        public Persona( string nombre , string apellido , int edad) {
+
+            Nombre = nombre;
+            Apellido = apellido;
+            Edad = edad;
+        }
+
+        public void Saludar ()
+        {
+            Console.WriteLine("saludos {0} de apellido {1} como estas", Nombre, Apellido);
+        }
+    }
+}
---
+++ C#/clase persona/clase_persona(reto2)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_persona_reto2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona estiben = new Persona("Stiven" , "de la rosa" , 15);
+
+            estiben.Saludar();
+        }
+    }
+}
