60db9cca5cc34a1a418dcb81d045e99fa5fac343
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2025-01-05 14:34:42 -0400

    caras y cruses

--- Python/Cara o Cruz/index.py
+++ Python/Cara o Cruz/index.py
@@ -1,14 +1,31 @@
-# Programa un lanzamiento de cara o cruz que este trucado para que lanzo mas veces cara
 import random
-
-
 
 # Función que simula el lanzamiento de la moneda
 moneda = ["cara", "cruz"]
 def lanzar_moneda():
-    if random.random() < 0.6:
+    if random.random() < 0.6:  # Probabilidad de 60% para "cara"
         return moneda[0]
     else:  
         return moneda[1]
 
-print(lanzar_moneda())
+
+print(f"ha salido {lanzar_moneda()}")
+caras = 0
+cruces = 0
+
+# Realizar los lanzamientos
+for i in range(1000000):
+    if lanzar_moneda() == "cara":
+        caras += 1
+    else:
+        cruces += 1
+
+# Calcular porcentajes
+total = caras + cruces
+porcentaje_caras = (caras / total) * 100
+porcentaje_cruces = (cruces / total) * 100
+
+# Mostrar resultados
+print("Caras:", caras, "Cruces:", cruces)
+print(f"Porcentaje de caras: {porcentaje_caras:.2f}%")
+print(f"Porcentaje de cruces: {porcentaje_cruces:.2f}%")
