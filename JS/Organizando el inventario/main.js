b83726ca245d9f7948294b5b28f9a3458855eabb
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2025-04-02 22:40:22 -0400

    Reto #3 🏗️ Organizando el inventario

---
+++ JS/Organizando el inventario/main.js
@@ -0,0 +1,38 @@
+function organizeInventory(inventory) {
+    let organized = {}; // Objeto para almacenar los datos organizados
+
+    inventory.forEach(({ name, quantity, category }) => {
+        // Si la categoría no existe, la creamos
+        if (!organized[category]) {
+            organized[category] = {};
+        }
+
+        // Si el juguete ya está en la categoría, sumamos su cantidad
+        if (organized[category][name]) {
+            organized[category][name] += quantity;
+        } else {
+            organized[category][name] = quantity;
+        }
+    });
+
+    return organized;
+}
+
+
+const inventory = [
+    { name: 'doll', quantity: 5, category: 'toys' },
+    { name: 'car', quantity: 3, category: 'toys' },
+    { name: 'ball', quantity: 2, category: 'sports' },
+    { name: 'car', quantity: 2, category: 'toys' },
+    { name: 'racket', quantity: 4, category: 'sports' }
+];
+
+console.log(organizeInventory(inventory));
+
+const inventory2 = [
+    { name: 'book', quantity: 10, category: 'education' },
+    { name: 'book', quantity: 5, category: 'education' },
+    { name: 'paint', quantity: 3, category: 'art' }
+];
+
+console.log(organizeInventory(inventory2));
