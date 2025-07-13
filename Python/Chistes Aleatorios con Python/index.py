cf0cf844a65a41c74f5906e199f19730493e215f
Stiven de la rosa <108486081+codestiven@users.noreply.github.com>
2025-07-12 22:59:43 -0400

    solucionado

--- Python/Chistes Aleatorios con Python/index.py
+++ Python/Chistes Aleatorios con Python/index.py
@@ -4,17 +4,17 @@ def get_random_joke():
     url = "https://icanhazdadjoke.com/"
     headers = {
         "Accept": "application/json",
-        "User-Agent": "Practica-Python (https://github.com/tu-usuario)"
+        "User-Agent": "Python-App"
     }
 
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
-        print("\n🃏 Aquí tienes un chiste aleatorio:\n")
-        print(f"💬 {data['joke']}\n")
+        print("\nAquí tienes un chiste aleatorio:\n")
+        print(f"{data['joke']}\n")
     else:
-        print("❌ Ocurrió un error al obtener el chiste.")
+        print("Ocurrió un error al obtener el chiste.")
 
 if __name__ == "__main__":
     get_random_joke()
